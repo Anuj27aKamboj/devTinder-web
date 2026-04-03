7562db23aa96ecdf1cff04719373824b59d39ca1
Anuj Kamboj <anujkamboj.27@Anujs-MacBook-Air.local>
2026-04-04 00:22:29 +0530

    Added Navbar, FOoter and Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Components/Body";
 
+function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+          <Route path="/login" element={<div>Login</div>}/>
+          <Route path="/profile" element={<div>Profile</div>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+      {/* <Theme /> */}
     </>
   );
 }
