0fcd762ef8d8c9ad63c15ab436943c6a47608194
Anuj Kamboj <anujkamboj.27@Anujs-MacBook-Air.local>
2026-04-22 19:05:11 +0530

    Updated paths

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "/api";
+export const BASE_URL = location.hostname === "localhost"? "http://localhost:7777":"/api";
 
 export const DEFAULT_AVATAR = "https://pixabay.com/vectors/blank-profile-picture-mystery-man-973460/";
